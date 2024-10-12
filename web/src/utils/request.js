868449d9442d7f56c4fb4669f5ca46108c60279f
malong <utokato@outlook.com>
2024-10-12 16:41:04 +0800

    some details

--- web/src/utils/request.js
+++ web/src/utils/request.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 // 创建axios实例
 const service = axios.create({
-    //baseURL: window.location.origin,
-    baseURL: "http://localhost:10321",
+    baseURL: window.location.origin,
+    // baseURL: "http://localhost:10321",
     // 超时
     timeout: 10000
 })
